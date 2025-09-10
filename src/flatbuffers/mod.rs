5ac1c9542d33caaa04ed8b3fe6f1b77555903b4c
xpcn <xpcn2015@gmail.com>
2025-09-10 08:52:31 +0700

    refactor: move tcrm_task_generated export to flatbuffers module

--- src/flatbuffers/mod.rs
+++ src/flatbuffers/mod.rs
@@ -2,3 +2,5 @@ pub mod conversion;
 #[allow(dead_code, unused_imports)]
 #[path = "tcrm_monitor_generated.rs"]
 pub mod tcrm_monitor_generated;
+
+pub use tcrm_task::flatbuffers::tcrm_task_generated;
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,4 @@
 pub mod flatbuffers;
 pub mod monitor;
 
-#[cfg(feature = "flatbuffers")]
-pub use tcrm_task::flatbuffers::tcrm_task_generated;
-
 pub use tcrm_task::tasks as tcrm_tasks;
